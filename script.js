8761a953684ed6770ceb0f6958fad77b015209f1
Felipe Mendes <luizfelipe.m93@gmail.com>
2023-05-24 18:20:55 -0300

    animação no scroll

--- script.js
+++ script.js
@@ -61,3 +61,26 @@ function initSmoothScroll() {
     });
 }
 initSmoothScroll();
+
+function initAnimateScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+
+    if (sections.length) {
+        const halfWindow = window.innerHeight * 0.6;
+
+        function animateScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = sectionTop - halfWindow < 0;
+                if (isSectionVisible) {
+                    section.classList.add('ativo');
+                } else {
+                    section.classList.remove('ativo');
+                }
+            });
+        }
+        animateScroll();
+        window.addEventListener('scroll', animateScroll)
+    }
+}
+initAnimateScroll();
