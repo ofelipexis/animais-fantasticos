dfaabaa1e26a9dd62afa29d453e8a266096c8bc5
Felipe Mendes <luizfelipe.m93@gmail.com>
2023-06-02 17:49:37 -0300

    separando scripts por módulos

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initAccordion from "./modules/accordion.js";
+import initAnimateScroll from "./modules/animate-scroll.js";
+import initSmoothScroll from "./modules/smooth-scroll.js";
+import initTabNav from "./modules/tab-nav.js";
+
+initAccordion();
+initAnimateScroll();
+initSmoothScroll();
+initTabNav();
