4a405e8c1546e441ad8d31a89d05edebb8b824f3
Felipe Mendes <luizfelipe.m93@gmail.com>
2023-06-07 15:40:49 -0300

    desenvolvimento de função para ativar e desativar o Modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initAccordion from "./modules/accordion.js";
 import initAnimateScroll from "./modules/animate-scroll.js";
 import initSmoothScroll from "./modules/smooth-scroll.js";
 import initTabNav from "./modules/tab-nav.js";
+import initModal from "./modules/modal.js";
 
 initAccordion();
 initAnimateScroll();
 initSmoothScroll();
 initTabNav();
+initModal();
