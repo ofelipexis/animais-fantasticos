c748bfa853b00a96ba375b1015c1bb68d3d3b5c2
Felipe Mendes <luizfelipe.m93@gmail.com>
2023-06-13 17:28:14 -0300

    adicionando dropdown menu e criando function para outsideclick

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initSmoothScroll from "./modules/smooth-scroll.js";
 import initTabNav from "./modules/tab-nav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
 
 initAccordion();
 initAnimateScroll();
@@ -11,3 +12,4 @@ initSmoothScroll();
 initTabNav();
 initModal();
 initTooltip();
+initDropdownMenu();
